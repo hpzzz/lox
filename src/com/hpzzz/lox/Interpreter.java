8ddd24500f3e3bc19c9e544e1cb4c0ec25fb400f
hpzzz <karol0404@gmail.com>
2024-12-21 19:19:38 +0100

    running interpreter

--- src/com/hpzzz/lox/Interpreter.java
+++ src/com/hpzzz/lox/Interpreter.java
@@ -1,22 +1,51 @@
 package com.hpzzz.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error){
+            Lox.runtimeError(error);
+
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                // discard end since precision is floating no double
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
         Object right = evaluate(expr.right);
         switch (expr.operator.type) {
             case GREATER:
+                checkNumbersOperand(expr.operator, left, right);
                 return (double)left > (double)right;
             case GREATER_EQUAL:
+                checkNumbersOperand(expr.operator, left, right);
                 return (double)left >= (double)right;
             case LESS:
+                checkNumbersOperand(expr.operator, left, right);
                 return (double)left < (double)right;
             case LESS_EQUAL:
+                checkNumbersOperand(expr.operator, left, right);
                 return (double)left <= (double)right;
             case BANG_EQUAL: return !isEqual(left, right);
             case EQUAL_EQUAL: return isEqual(left, right);
             case MINUS:
+                checkNumbersOperand(expr.operator, left, right);
                 return (double)left - (double)right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
@@ -25,14 +54,17 @@ public class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof String && right instanceof String) {
                     return (String)left + (String)right;
                 }
-                break;
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings");
             case SLASH:
+                checkNumbersOperand(expr.operator, left, right);
                 return (double)left / (double)right;
             case STAR:
+                checkNumbersOperand(expr.operator, left, right);
                 return (double)left * (double)right;
         }
         return null;
     }
+
 
     private boolean isEqual(Object a, Object b) {
         if (a == null && b == null) return true;
@@ -52,7 +84,7 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     @Override
     public Object visitTernaryExpr(Expr.Ternary expr) {
-        return null;
+        return isTruthy(evaluate(expr.condition)) ? evaluate(expr.thenBranch) : evaluate(expr.elseBranch);
     }
 
     @Override
@@ -60,6 +92,7 @@ public class Interpreter implements Expr.Visitor<Object> {
         Object right = evaluate(expr.right);
         switch (expr.operator.type) {
             case MINUS:
+                checkRightOperand(expr.operator, expr.right);
                 return -(double)right;
             case BANG:
                 return !isTruthy(right);
@@ -67,8 +100,18 @@ public class Interpreter implements Expr.Visitor<Object> {
         return null;
     }
 
+    private void checkRightOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number");
+    }
+
+    private void checkNumbersOperand(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers");
+    }
+
     private boolean isTruthy(Object object) {
-        if (object == nulll) return false;
+        if (object == null) return false;
         if (object instanceof Boolean) return (boolean) object;
         return true;
     }
