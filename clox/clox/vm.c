29cfacab21b8a4df76ac111107fe210855ce430b
hpzzz <harasimkar@gmail.com>
2025-02-10 00:39:51 +0100

    init compiling expressions with pratt parser

--- clox/clox/vm.c
+++ clox/clox/vm.c
@@ -9,6 +9,7 @@
 #include "vm.h"
 #include <stdio.h>
 #include "debug.h"
+#include "compiler.h"
 
 VM vm;
 
@@ -86,7 +87,20 @@ static InterpretResult run(void) {
 }
 
 InterpretResult interpret(const char* source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+    
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+    
+    vm.chunk = &chunk;
+    vm.ip  = vm.chunk->code;
+    
+    InterpretResult result = run();
+    
+    freeChunk(&chunk);
+    return result;
 }
 
