1f5113eff3266a377a94e8028aed4304a9453b1b
hpzzz <harasimkar@gmail.com>
2025-02-11 22:24:09 +0100

    Introduce Strings

--- clox/clox/compiler.c
+++ clox/clox/compiler.c
@@ -12,6 +12,7 @@
 #include "debug.h"
 #endif
 #include <stdlib.h>
+#include "object.h"
 
 typedef struct {
     Token current;
@@ -183,6 +184,10 @@ static void number(void) {
     emitConstant(NUMBER_VAL(value));
 }
 
+static void string(void) {
+    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
 static void unary(void) {
     TokenType operatorType = parser.previous.type;
     
@@ -217,7 +222,7 @@ ParseRule rules[] = {
   [TOKEN_LESS]          = {NULL,     binary, PREC_COMPARISON},
   [TOKEN_LESS_EQUAL]    = {NULL,     binary, PREC_COMPARISON},
   [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_STRING]        = {string,   NULL,   PREC_NONE},
   [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
   [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
   [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
--- clox/clox/value.c
+++ clox/clox/value.c
@@ -5,9 +5,11 @@
 //  Created by Karol Harasim on 2/4/25.
 //
 
+#include <string.h>
 #include "value.h"
 #include "memory.h"
 #include <stdio.h>
+#include "object.h"
 
 void initValueArray(ValueArray* array) {
     array->count = 0;
@@ -36,6 +38,7 @@ void printValue(Value value) {
           break;
       case VAL_NIL: printf("nil"); break;
       case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+      case VAL_OBJ:    printObject(value); break;
     }
 }
 
@@ -46,6 +49,12 @@ bool valuesEqual(Value a, Value b) {
         case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
         case VAL_NIL:    return true;
         case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        case VAL_OBJ: {
+            ObjString* aString = AS_STRING(a);
+            ObjString* bString = AS_STRING(b);
+            return aString->length == bString->length &&
+            memcmp(aString->chars, bString->chars, aString->length) == 0;
+        }
         default:         return false;
     }
 }
